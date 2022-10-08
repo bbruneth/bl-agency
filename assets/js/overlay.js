63c3551c6a4751816348375d2e8e4e093d8d2de9
bbruneth <79019573+bbruneth@users.noreply.github.com>
2022-10-08 21:50:00 +0100

    first commit

---
+++ assets/js/overlay.js
@@ -0,0 +1,9 @@
+let overlay = document.querySelector('.menu_overlay');
+
+function activeOverlay() {
+    overlay.classList.add('active');
+}
+
+function offOverlay() {
+    overlay.classList.remove('active');
+}
