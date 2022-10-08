384a9ff1bdb5a526cce89f4ba47c0e8e68b9fdb8
bbruneth <79019573+bbruneth@users.noreply.github.com>
2022-10-08 22:55:49 +0100

    Add files via upload

--- assets/js/transition.js
+++ assets/js/transition.js
@@ -35,3 +35,37 @@ barba.init({
     }]
 
 })
+
+barba.hooks.after(() => {
+	let texto1 = document.querySelector('.texto1');
+let texto2 = document.querySelector('.texto2');
+let texto3 = document.querySelector('.texto3');
+
+function rotateWords() {
+    texto1.classList.add('is-visible');
+
+    setTimeout(() => {
+        texto1.classList.remove('is-visible');
+        texto1.classList.add('is-hidden');
+        texto2.classList.add('is-visible');
+        texto3.classList.remove('is-hidden');
+    }, 2000);
+
+    setTimeout(() => {
+        texto1.classList.remove('is-hidden');
+        texto2.classList.remove('is-visible');
+        texto2.classList.add('is-hidden');
+        texto3.classList.add('is-visible');
+    }, 4000);
+
+    setTimeout(() => {
+        texto2.classList.remove('is-hidden');
+        texto3.classList.remove('is-visible');
+        texto3.classList.add('is-hidden');
+    }, 6000);
+}
+
+rotateWords();
+
+setInterval(rotateWords, 6000);
+});
